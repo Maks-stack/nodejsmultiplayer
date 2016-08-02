d65eb12c3b27fbd0cbca9a20f3a8eafeb1aa8e66
Maximilian Menschel <elmaxo@hotmail.de>
2016-08-03 01:08:53 +0200

    favicon try fix

--- Game.js
+++ Game.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var serv = require('http').Server(app);
 var port = process.env.PORT || 2000;
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/client/index.html');
@@ -7,12 +8,8 @@ app.get('/', function(req, res){
 
 app.use('/client', express.static(__dirname + '/client'));
 
-app.listen(port); //BLA
+serv.listen(port); //BLA
 console.log("server started");
-
-app.listen(port, function () {
-    console.log('Example app listening on port '+ port +'!');
-});
 
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
