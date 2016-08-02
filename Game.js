9c5528ebe571a3eba0fda887b3ce9a2375e2fa4f
Maximilian Menschel <elmaxo@hotmail.de>
2016-08-03 00:54:22 +0200

    changed port variable

--- Game.js
+++ Game.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
-var serv = require('http').Server(app);
-
+var port = process.env.PORT || 2000;
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/client/index.html');
 });
 
 app.use('/client', express.static(__dirname + '/client'));
 
-serv.listen(2000 || process.env.PORT); //BLA
+app.listen(port); //BLA
 console.log("server started");
+
+app.listen(port, function () {
+    console.log('Example app listening on port '+ port +'!');
+});
 
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
