0180ce974bfdb2393bf3bd3136610cc309f133f3
Maximilian Menschel <elmaxo@hotmail.de>
2016-08-05 00:56:19 +0200

    added socket.io

--- game.js
+++ game.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var favicon = require('serve-favicon');
 var app = express();
-var PORT = process.env.PORT || 5000;
+var server = require('http').Server(app);
+var PORT = process.env.PORT || 2000;
 
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
+app.get('/', function(req,res){
+	res.sendFile(__dirname + '/public/index.html');
+});
+app.use('/client', express.static(__dirname + '/client'));
 
-app.use(express.static(__dirname + '/public'));
-
-app.listen(PORT, function(){
+server.listen(PORT, function(){
     console.log("server started on port: " + PORT);
+});
+
+var io = require('socket.io')(server,{});
+io.sockets.on('connection', function(socket){
+    console.log('socket connection');
+
+    socket.emit('serverMsg', {
+    	msg: 'hello',
+    });
+
+    io.sockets.on('bla', function(data){
+    console.log(data.msg);
+
+    });
 });
