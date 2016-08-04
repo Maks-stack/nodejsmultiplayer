93a831f0e983df1be734103b9a33cd27275a988f
Maximilian Menschel <elmaxo@hotmail.de>
2016-08-04 22:08:24 +0200

    added require favicon

--- Game.js
+++ Game.js
@@ -1,11 +1,21 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+var middleware = {
+  
+    requireAuthentication: function (req, res,next){
+        console.log('private route hit');
+        next();
+    }
+};
+
+app.use(middleware.requireAuthentication);
 
 var port = process.env.PORT || 2000;
 
 console.log(__dirname);
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, function(){
     console.log("server started on port: " + port);
