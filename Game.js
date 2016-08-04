cac494c0e8b3f204181e403ce4f9650e87d4201b
Maximilian Menschel <elmaxo@hotmail.de>
2016-08-04 21:23:24 +0200

    removed all

--- Game.js
+++ Game.js
@@ -1,19 +1,11 @@
-//Lets require/import the HTTP module
-var http = require('http');
+var express = require("express");
+var app = express();
 
-//Lets define a port we want to listen to
-const PORT= process.env.PORT || 200; 
-
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+app.get('/', function(req, res){
+    
+    res.send('Hello Express!');
 });
+
+var port = process.env.PORT || 2000;
+
+app.listen(port);
