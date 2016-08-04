979c24aa9c4f2f9d6000a69da76a4395a94d9783
Maximilian Menschel <elmaxo@hotmail.de>
2016-08-04 23:44:30 +0200

    hello world

--- game.js
+++ game.js
@@ -1,33 +1,33 @@
 var express = require('express');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var app = express();
-var PORT = process.env.PORT || 2000;
 
-var middleware = {
-  
-    requireAuthentication: function (req, res,next){
-        console.log('private route hit');
-        next();
-    },
-    logger: function(req, res, next){
-        console.log(req.method + ' ' + req.originalUrl);
-        next();
-    }
-};
+//var middleware = {
+//  
+//    requireAuthentication: function (req, res,next){
+//        console.log('private route hit');
+//        next();
+//    },
+//    logger: function(req, res, next){
+//        console.log(req.method + ' ' + req.originalUrl);
+//        next();
+//    }
+//};
 
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+//app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
-app.use(middleware.logger);
+//app.use(middleware.logger);
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function (req, res){
     res.send('Hello World!');
-})
+});
 
+//console.log(__dirname);
 
+//app.use(express.static(__dirname + '/public'));
 
-console.log(__dirname);
-
-app.use(express.static(__dirname + '/public'));
-
-app.listen(PORT, function(){
-    console.log("server started on port: " + PORT);
+app.listen(app.get('port'), function(){
+    console.log("server started on port: " + app.get('port'));
 });
