e7d449c4b498d486f2074dba5a0f0f13f9fa58a6
Maximilian Menschel <elmaxo@hotmail.de>
2016-08-04 20:58:31 +0200

    removed part of code from game.js

--- Game.js
+++ Game.js
@@ -2,16 +2,7 @@ var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 var port = process.env.PORT;
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/client/index.html');
-});
-
-app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(port); //BLA
 console.log("server started");
 
-var io = require('socket.io')(serv,{});
-io.sockets.on('connection', function(socket){
-    console.log('socket connection');
-});
