5ebde99b8fcc576ec0c7bcfed8528f2c07ea6c4e
Maximilian Menschel <elmaxo@hotmail.de>
2016-08-04 23:04:51 +0200

    tryfix

--- Game.js
+++ Game.js
@@ -1,22 +1,30 @@
-var express = require("express");
+var express = require('express');
+var favicon = require('serve-favicon');
 var app = express();
+var PORT = process.env.PORT || 2000;
 
 var middleware = {
   
     requireAuthentication: function (req, res,next){
         console.log('private route hit');
         next();
+    },
+    logger: function(req, res, next){
+        console.log(req.method + ' ' + req.originalUrl);
+        next();
     }
 };
 
-app.use(middleware.requireAuthentication);
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
-var port = process.env.PORT || 2000;
+app.use(middleware.logger);
+
+
 
 console.log(__dirname);
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, function(){
-    console.log("server started on port: " + port);
+app.listen(PORT, function(){
+    console.log("server started on port: " + PORT);
 });
